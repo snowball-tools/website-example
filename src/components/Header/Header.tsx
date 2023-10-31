68110850c04ce9dca0f41c13768a42537f03717d
Joao Stein <joaosteing@gmail.com>
2023-10-30 21:33:54 -0300

    feat: add `/` link to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import Nav from '../Nav';
 import { CtaSecondary } from '../Cta';
@@ -7,7 +8,9 @@ const Header = () => {
   return (
     <>
       <header className="relative p-2 sm:p-4 flex flex-row-reverse md:flex-row justify-between max-w-7xl mx-auto items-center">
-        <Image className={style.logo} src="/macro.svg" alt="Macro Logo" width={180} height={37} priority />
+        <Link href="/">
+          <Image className={style.logo} src="/macro.svg" alt="Macro Logo" width={180} height={37} priority />
+        </Link>
         <Nav />
         <div className="hidden md:block">
           <CtaSecondary href="#">Request an Audit</CtaSecondary>
