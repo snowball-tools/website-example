2cd01ccb4e7633b22ab9b1f0bea7c92119d06b05
Joao Stein <joaosteing@gmail.com>
2023-10-30 22:02:44 -0300

    fix: reduce max-width of the Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import style from './Header.module.scss';
 const Header = () => {
   return (
     <>
-      <header className="relative p-2 sm:p-4 flex flex-row-reverse md:flex-row justify-between max-w-7xl mx-auto items-center">
+      <header className="relative p-2 sm:p-4 flex flex-row-reverse md:flex-row justify-between max-w-5xl mx-auto items-center">
         <Link href="/">
           <Image className={style.logo} src="/macro.svg" alt="Macro Logo" width={180} height={37} priority />
         </Link>
