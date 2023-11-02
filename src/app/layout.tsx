5629147896e2cc4132dd023e4dbf3c994084c34e
Joao Stein <joaosteing@gmail.com>
2023-11-02 19:09:29 -0300

    feat: add open graph metadata and URL base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,12 @@ import '../styles/globals.scss';
 export const metadata: Metadata = {
   title: 'Macro',
   description: 'Macro is trusted by multiple leading industry names in Defi and Crypto for Smart Contract auditing.',
+  metadataBase: new URL('https://macro-website.onrender.com'),
+  openGraph: {
+    title: '0xMacro.com',
+    description: 'Macro is trusted by multiple leading industry names in Defi and Crypto for Smart Contract auditing.',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
