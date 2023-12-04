53c8ead251d5561fc8f118eaf3c48038028d7107
Joao Stein <joaosteing@gmail.com>
2023-12-04 20:03:20 -0300

    fix: implement active state on navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,37 +3,39 @@
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const navigation = [
-  { name: 'Audits', href: '/', current: true },
-  { name: 'Bots', href: '/bots', current: false },
-  { name: 'Audit Library', href: 'https://0xmacro.com/library', current: false, newTab: true },
-  { name: 'Blog', href: 'https://0xmacro.com/blog/', current: false, newTab: true },
+  { name: 'Audits', href: '/' },
+  { name: 'Bots', href: '/bots' },
+  { name: 'Audit Library', href: 'https://0xmacro.com/library', newTab: true },
+  { name: 'Blog', href: 'https://0xmacro.com/blog/', newTab: true },
 ]
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-function getNavItems() {
+function getNavItems(currentPath: string) {
   return navigation.map((item) => (
     <Disclosure.Button key={item.name} as="li">
       <Link
         href={item.href}
-        className={classNames(
-          item.current ? '' : 'text-black',
-          'block px-3 py-2 text-lg hover:text-black transition-colors duration-300 md:text-sm lg:text-base',
-        )}
-        aria-current={item.current ? 'page' : undefined}
+        className={'block px-3 py-2 text-lg transition-colors duration-300 md:text-sm lg:text-base'}
+        aria-current={currentPath === item.href ? 'page' : undefined}
         target={item.newTab ? '_blank' : undefined}
       >
-        {item.name}
+        <span className={currentPath === item.href ? 'gradient-text font-semibold' : 'text-black'}>
+          {item.name}
+        </span>
       </Link>
     </Disclosure.Button>
   ))
 }
 
 const Nav = () => {
+  const currentPath = usePathname()
+
   return (
     <Disclosure as="nav">
       {({ open }) => (
@@ -51,11 +53,13 @@ const Nav = () => {
             </div>
 
             <div className="hidden md:block">
-              <ul className="flex space-x-1 lg:space-x-4">{getNavItems()}</ul>
+              <ul className="flex space-x-1 lg:space-x-4">{getNavItems(currentPath)}</ul>
             </div>
 
             <Disclosure.Panel className="absolute z-10 top-[58px] left-0 right-0 md:hidden">
-              <ul className="space-y-1 px-2 pb-3 pt-2 bg-white shadow">{getNavItems()}</ul>
+              <ul className="space-y-1 px-2 pb-3 pt-2 bg-white shadow">
+                {getNavItems(currentPath)}
+              </ul>
             </Disclosure.Panel>
           </div>
         </>
