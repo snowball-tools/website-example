795dd7de136cb00ee67c36bdd1b294ad374a3fc0
Joao Stein <joaosteing@gmail.com>
2023-12-06 20:27:53 -0300

    feat: add dark theme support to `StandaloneHeading` component

--- src/components/StandaloneHeading/StandaloneHeading.tsx
+++ src/components/StandaloneHeading/StandaloneHeading.tsx
@@ -4,7 +4,7 @@ type StandaloneHeadingProps = {
 
 const StandaloneHeading = ({ children }: StandaloneHeadingProps) => {
   return (
-    <section className="max-w-sm mx-auto text-center text-gray-500">
+    <section className="max-w-sm mx-auto text-center text-gray-500 dark:text-white">
       <h1 className="text-3xl mb-16 font-mono sm:text-4xl sm:leading-normal">{children}</h1>
     </section>
   )
