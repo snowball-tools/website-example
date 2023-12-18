82c0179b198d45de5273742857af380ccf06bb6e
Joao Stein <joaosteing@gmail.com>
2023-12-18 16:58:54 -0300

    chore: replace description text

--- src/app/gas-blaster/layout.tsx
+++ src/app/gas-blaster/layout.tsx
@@ -4,11 +4,11 @@ import '../../styles/globals.scss'
 
 export const metadata: Metadata = {
   title: 'Macro - Gas Blaster',
-  description: 'Make your contracts gas efficient with a one-click Gas Audit for your contracts',
+  description: 'Make your contracts gas efficient. Get a one-click Gas Audit.',
   metadataBase: new URL('https://macro-website.onrender.com'),
   openGraph: {
     title: '0xMacro.com - Gas Blaster',
-    description: 'Make your contracts gas efficient with a one-click Gas Audit for your contracts',
+    description: 'Make your contracts gas efficient. Get a one-click Gas Audit.',
     type: 'website',
   },
 }
