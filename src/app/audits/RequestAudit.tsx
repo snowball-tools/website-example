97500f062bcac4a1db3a5b3fe2b72a300954f17a
emrekap <emrekap@hotmail.com>
2024-03-05 15:09:15 +0300

    request audit form text change

--- src/app/audits/RequestAudit.tsx
+++ src/app/audits/RequestAudit.tsx
@@ -11,7 +11,7 @@ export function RequestAudit({ formId }: Props) {
       id={formId}
       className="rounded-md bg-[#1C44A9] dark:bg-dusk-50 dark:hover:bg-white px-3.5 py-2.5 text-sm font-semibold text-white dark:text-night-950 shadow-sm hover:shadow-lg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-600 transition"
     >
-      Open Inquiry Form
+      Submit an Inquiry
     </PopupButton>
   )
 }
